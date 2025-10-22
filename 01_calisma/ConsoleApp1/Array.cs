6df478cc5e4a8b7b2d9572e5d5c09a1c3c0e326c
nazl1 <karnalnazli@gmail.com>
2025-10-22 16:31:58 +0300

    sync files

---
+++ 01_calisma/ConsoleApp1/Array.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Array
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Array elements:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
